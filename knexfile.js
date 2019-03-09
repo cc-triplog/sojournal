789314007bf3bdbb092c0550cf86bfb207f04189
Chaz Wilson <chaz303@yahoo.com>
2019-03-09 11:41:37 +0900

    added dotenv, edited knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,37 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'pg',
-    connection:'postgres://localhost/triplog',
+    client: "pg",
+    connection: {
+      host: "localhost",
+      //user: '',
+      //password: '',
+      database: "triplog"
+    },
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds'
+      directory: "./db/seeds"
     },
     useNullAsDefault: true
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "triplog"
+    },
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: './db/seeds'
+      directory: "./db/seeds"
     },
     useNullAsDefault: true
   }
-}
+};
