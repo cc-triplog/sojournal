59bb80040deab43d2fdc34d64d6eb2db4dac6060
okalouti <kalouo@pm.me>
2019-03-16 19:16:16 +0900

    refactors code to add device permissions interface

--- screens/Camera.js
+++ screens/Camera.js
@@ -18,92 +18,120 @@ export default class CameraPage extends React.Component {
   state = {
     capture: null,
     imageView: false,
-    modalVisible: false
+    modalVisible: false,
+    permissions: {
+      hasLocationPermission: null,
+      hasCameraPermission: null
+    }
   };
 
   checkPermissions = async () => {
-    const camera = await Permissions.askAsync(Permissions.CAMERA);
-    const hasCameraPermission = camera.status === "granted";
+    // const camera = await Permissions.askAsync(Permissions.CAMERA);
+    // const hasCameraPermission = camera.status === "granted";
 
-    const cameraRoll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    const hasCameraRollPermission = cameraRoll.status === "granted";
+    // const cameraRoll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    // const hasCameraRollPermission = cameraRoll.status === "granted";
 
-    const location = await Permissions.askAsync(Permissions.LOCATION);
-    const hasLocationPermission = location.status === "granted";
+    // const location = await Permissions.askAsync(Permissions.LOCATION);
+    // const hasLocationPermission = location.status === "granted";
 
-    return (
-      hasCameraPermission && hasCameraRollPermission && hasLocationPermission
-    );
+    // return (
+    //   hasCameraPermission && hasCameraRollPermission && hasLocationPermission
+    // );
+    const {
+      hasLocationPermission,
+      hasCameraPermission
+    } = this.state.permissions;
+    return hasLocationPermission && hasCameraPermission;
   };
 
   getDateFromCamera = input => {
     const separators = new RegExp("[: ]", "g");
+    if (!input) return new Date().getTime();
     return new Date(...input.split(separators)).getTime();
   };
 
   launchCamera = async () => {
-    if (this.checkPermissions) {
-      await ImagePicker.launchCameraAsync({
-        base64: true,
-        exif: true
-      }).then(async res => {
-        let capture;
-        if (!res.cancelled) {
-          if (Platform.OS === "ios") {
-            const location = await Location.getCurrentPositionAsync({});
-            capture = {
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-              base64: res.base64,
-              timestamp: this.getDateFromCamera(res.exif.DateTimeDigitized),
-              uri: res.uri
-            };
-          } else {
-            capture = {
-              latitude: res.exif.GPSLatitude,
-              longitude: res.exif.GPSLongitude,
-              base64: res.base64,
-              timestamp: this.getDateFromCamera(res.exif.DateTime),
-              uri: res.uri
-            };
-          }
-          this.setState({ capture, imageView: true });
+    await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.CAMERA_ROLL,
+      Permissions.LOCATION
+    )
+      .then(async res => {
+        if (res.status !== "granted") {
+          return alert(
+            "The application needs access to your camera and current location in order to take photos and geo-locate them for your use."
+          );
+        } else {
+          await ImagePicker.launchCameraAsync({
+            base64: true,
+            exif: true
+          }).then(async res => {
+            let capture;
+            if (!res.cancelled) {
+              if (Platform.OS === "ios") {
+                const location = await Location.getCurrentPositionAsync({});
+                capture = {
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                  base64: res.base64,
+                  timestamp: this.getDateFromCamera(res.exif.DateTimeDigitized),
+                  uri: res.uri
+                };
+              } else {
+                capture = {
+                  latitude: res.exif.GPSLatitude,
+                  longitude: res.exif.GPSLongitude,
+                  base64: res.base64,
+                  timestamp: this.getDateFromCamera(res.exif.DateTime),
+                  uri: res.uri
+                };
+              }
+              this.setState({ capture, imageView: true });
+            }
+          });
         }
+      })
+      .catch(error => {
+        throw error;
       });
-    }
   };
 
   launchLibrary = async () => {
-    if (this.checkPermissions) {
-      await ImagePicker.launchImageLibraryAsync({
-        type: "Images",
-        base64: true,
-        exif: true
-      }).then(async res => {
-        let capture;
-        if (!res.cancelled) {
-          if (Platform.OS === "ios") {
-            const location = await Location.getCurrentPositionAsync({});
-            capture = {
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-              base64: res.base64,
-              timestamp: this.getDateFromCamera(res.exif.DateTimeDigitized),
-              uri: res.uri
-            };
-          } else {
-            capture = {
-              latitude: res.exif.GPSLatitude,
-              longitude: res.exif.GPSLongitude,
-              base64: res.base64,
-              timestamp: this.getDateFromCamera(res.exif.DateTime),
-              uri: res.uri
-            };
+    await Permissions.askAsync(Permissions.CAMERA_ROLL).then(async res => {
+      if (res.status === "granted") {
+        await ImagePicker.launchImageLibraryAsync({
+          type: "Images",
+          base64: true,
+          exif: true
+        }).then(async res => {
+          let capture;
+          if (!res.cancelled) {
+            if (Platform.OS === "ios") {
+              const location = await Location.getCurrentPositionAsync({});
+              capture = {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                base64: res.base64,
+                timestamp: this.getDateFromCamera(res.exif.DateTimeDigitized),
+                uri: res.uri
+              };
+            } else {
+              capture = {
+                latitude: res.exif.GPSLatitude,
+                longitude: res.exif.GPSLongitude,
+                base64: res.base64,
+                timestamp: this.getDateFromCamera(res.exif.DateTime),
+                uri: res.uri
+              };
+            }
+            this.setState({ capture, imageView: true });
           }
-          this.setState({ capture, imageView: true });
-        }
-      });
-    }
+        });
+      } else {
+        alert("Access to the library is required in order to upload a photo.");
+      }
+    });
   };
 
   trashPicture = () => {
@@ -144,10 +172,7 @@ export default class CameraPage extends React.Component {
     this.setModalVisible();
   };
 
-  async componentDidMount() {
-    await Permissions.askAsync(Permissions.CAMERA);
-    await Permissions.askAsync(Permissions.CAMERA_ROLL);
-  }
+  async componentDidMount() {}
 
   render() {
     const { capture, imageView, modalVisible } = this.state;
