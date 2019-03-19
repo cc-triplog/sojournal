01c1f44a6d1a2feb5d9b34bb771ac2fe61d974dc
Omar Kalouti <okalouti@gmail.com>
2019-03-19 10:54:32 +0900

    Revert "Amplify & Multi-User Basic"

--- App.js
+++ App.js
@@ -1,71 +1,17 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import reducer from "./reducer/index";
-import { AppLoading, Asset, Font, Icon } from "expo";
-import AppNavigator from "./navigation/AppNavigator";
-import axios from "axios";
-
-import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify from "@aws-amplify/core";
-import config from "./aws-exports";
-Amplify.configure(config);
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import reducer from './reducer/index'
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
 
 const store = createStore(reducer);
 
-class App extends React.Component {
-  state = { res: null };
-
-  userExists = async user => {
-    let exists = null;
-    await axios({
-      url:
-        "http://ec2-54-199-164-132.ap-northeast-1.compute.amazonaws.com:4000/graphql",
-      method: "post",
-      data: {
-        query: `query
-        {
-          ReadUser(type: {email: "${user}"}) {
-          id
-          }
-      }`
-      }
-    }).then(res => {
-      exists = res.data.data.ReadUser.length > 0;
-    });
-    return exists;
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
   };
-
-  createUser = async (email, name) => {
-    axios({
-      url:
-        "http://ec2-54-199-164-132.ap-northeast-1.compute.amazonaws.com:4000/graphql",
-      method: "post",
-      data: {
-        query: `mutation
-          {CreateUser(
-            input:{
-              email:"${email}"
-              name:"${name}"
-          })}`
-      }
-    });
-  };
-
-  async componentDidMount() {
-    if (this.props.authState === "signedIn") {
-      const exists = await this.userExists(
-        this.props.authData.attributes.email
-      );
-      if (!exists) {
-        await this.createUser(
-          this.props.authData.attributes.email,
-          this.props.authData.attributes.sub
-        );
-      } else return;
-    }
-  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -78,9 +24,9 @@ class App extends React.Component {
       );
     } else {
       return (
-        <Provider store={store}>
+        <Provider store={ store }>
           <View style={styles.container}>
-            {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
             <AppNavigator />
           </View>
         </Provider>
@@ -91,16 +37,16 @@ class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require("./assets/images/robot-dev.png"),
-        require("./assets/images/robot-prod.png")
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
-      })
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
     ]);
   };
 
@@ -118,8 +64,6 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
-  }
+    backgroundColor: '#fff',
+  },
 });
-
-export default withAuthenticator(App, { includeGreetings: true });
