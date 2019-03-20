42002c7d5af16214e4d4fcacfeb709643f4eb52e
okalouti <kalouo@pm.me>
2019-03-20 12:01:26 +0900

    adds actions to set user ID and set image capture data

--- action/index.js
+++ action/index.js
@@ -1,71 +1,87 @@
-const changeCardVisibility = (visibility) => {
-    return {
-        type: "CHANGE_CARDVISIBILITY",
-        visibility
-    }
-}
-const changeTitle = (title) => {
-    return {
-        type: "CHANGE_TITLE",
-        title
-    }
-}
-const changeDescription = (description) => {
-    return {
-        type: "CHANGE_DESCRIPTION",
-        description
-    }
-}
-const insertPhotoWithIndex = (photo) => {
-    return {
-        type: "INSERT_PHOTOWITHINDEX",
-        photo
-    }
-}
-const deletePhoto = (index) => {
-    return {
-        type: "DELETE_PHOTO",
-        index
-    }
-}
-const renderGPS = (GPS) => {
-    return {
-        type: "RENDER_GPS",
-        GPS
-    }
-}
-const renderPhotos = (photos) => {
-    return {
-        type: "RENDER_PHOTOS",
-        photos
-    }
-}
-const selectImageCard = (index) => {
-    return {
-        type: "SELECT_IMAGECARD",
-        index
-    }
-}
-const reflectStateChange = (change) => {
-    return {
-        type: "REFLECT_STATECHANGE",
-        change
-    }
-}
-const replaceAllMarkers = (photos) => {
-    return {
-        type: "REPLACE_ALLMARKERS",
-        photos
-    }
-}
+const changeCardVisibility = visibility => {
+  return {
+    type: "CHANGE_CARDVISIBILITY",
+    visibility
+  };
+};
+const changeTitle = title => {
+  return {
+    type: "CHANGE_TITLE",
+    title
+  };
+};
+const changeDescription = description => {
+  return {
+    type: "CHANGE_DESCRIPTION",
+    description
+  };
+};
+const insertPhotoWithIndex = photo => {
+  return {
+    type: "INSERT_PHOTOWITHINDEX",
+    photo
+  };
+};
+const deletePhoto = index => {
+  return {
+    type: "DELETE_PHOTO",
+    index
+  };
+};
+const renderGPS = GPS => {
+  return {
+    type: "RENDER_GPS",
+    GPS
+  };
+};
+const renderPhotos = photos => {
+  return {
+    type: "RENDER_PHOTOS",
+    photos
+  };
+};
+const selectImageCard = index => {
+  return {
+    type: "SELECT_IMAGECARD",
+    index
+  };
+};
+const reflectStateChange = change => {
+  return {
+    type: "REFLECT_STATECHANGE",
+    change
+  };
+};
+const replaceAllMarkers = photos => {
+  return {
+    type: "REPLACE_ALLMARKERS",
+    photos
+  };
+};
 
-module.exports ={
-    renderPhotos,
-    changeCardVisibility,
-    selectImageCard,
-    insertPhotoWithIndex,
-    deletePhoto,
-    reflectStateChange,
-    replaceAllMarkers,
-    renderGPS
-}
+const setCapture = capture => {
+  return {
+    type: "SET_CAPTURE",
+    capture
+  };
+};
+
+const setUserId = id => {
+  return {
+    type: "SET_USER_ID",
+    id
+  };
+};
+
+module.exports = {
+  renderPhotos,
+  changeCardVisibility,
+  selectImageCard,
+  insertPhotoWithIndex,
+  deletePhoto,
+  reflectStateChange,
+  replaceAllMarkers,
+  renderGPS,
+  setCapture,
+  setUserId
+};
