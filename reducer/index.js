aad459efa11c29a80d88f892585ec64d4a932c00
okalouti <kalouo@pm.me>
2019-03-22 13:44:40 +0900

    commits after pull upstream

--- reducer/index.js
+++ reducer/index.js
@@ -16,13 +16,92 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "RENDER_PHOTO": {
-      const stateChanges = { markers: action.photo };
-      return {
-        ...state,
-        markers: [...state.markers, action.photo]
-      };
+    switch (action.type) {
+        case "RENDER_PHOTO": {
+            const stateChanges = { markers: action.photo };
+            return {
+                ...state,
+                markers: [...state.markers, action.photo]
+            };
+        }
+        case "RENDER_PHOTOS": {
+            const stateChanges = { markers: action.photos }
+            return {
+                ...state,
+                ...stateChanges
+            };
+        }
+        case "CHANGE_CARDVISIBILITY": {
+            const stateChanges = { visible: action.visibility };
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "SELECT_IMAGECARD": {
+            const stateChanges = { selectedImageIndex: action.index }
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "INSERT_PHOTOWITHINDEX": {
+            const stateChanges = {
+                markers: [
+                    ...state.markers.slice(0, state.selectedImageIndex + 1),
+                    action.photo,
+                    ...state.markers.slice(state.selectedImageIndex + 1)
+                ]
+            }
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "DELETE_PHOTO": {
+            const stateChanges = {
+                markers: [
+                    ...state.markers.slice(0, action.index - 1),
+                    ...state.markers.slice(action.index + 1)
+                ]
+            }
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "REFLECT_STATECHANGE": {
+            const stateChanges = { stateChanged: action.change }
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "REPLACE_ALLMARKERS": {
+            const stateChanges = { markers: action.photos }
+            return {
+                ...state,
+                ...stateChanges
+            }
+        }
+        case "RENDER_GPS": {
+            const stateChanges = { GPS: action.GPS }
+            return {
+                ...state,
+                GPS: [...state.GPS, action.GPS]
+            }
+        }
+        case "SET_CAPTURE": {
+            const stateChanges = { capture: action.capture };
+            return { ...state, ...stateChanges };
+        }
+        case "SET_USER_ID": {
+            const stateChanges = { userId: action.id };
+            return { ...state, ...stateChanges };
+        }
+        default: {
+            return state;
+        }
     }
     case "RENDER_PHOTOS": {
       const stateChanges = { markers: action.photos };
