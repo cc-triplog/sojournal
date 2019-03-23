4e2fddc258ad455317e6095d1ff5d971e2bc2dd2
okalouti <kalouo@pm.me>
2019-03-23 14:12:54 +0900

    implements create group functionality

--- components/CreateGroup.js
+++ components/CreateGroup.js
@@ -1,28 +1,128 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Button, Card } from "react-native-elements";
+import DateTimePicker from "react-native-modal-datetime-picker";
+import moment from "moment";
 
-export default () => (
-  <Card title="Create Trip">
-    <Text style={styles.inputTitle}> Title </Text>
-    <TextInput style={styles.title} placeholder="Title" />
-    <Text style={styles.inputTitle}> Description </Text>
-    <TextInput
-      style={styles.description}
-      placeholder="Description"
-      multiline={true}
-    />
-    <Text style={styles.inputTitle}> Date Range </Text>
-    <View style={styles.buttonContainer}>
-      <Button title="Start Date" buttonStyle={styles.button} />
-      <Button type="outline" buttonStyle={styles.button} disabled={true} />
-    </View>
-    <View style={styles.buttonContainer}>
-      <Button title="End Date" buttonStyle={styles.button} />
-      <Button type="outline" buttonStyle={styles.button} disabled={true} />
-    </View>
-  </Card>
-);
+//Redux
+import { connect } from "react-redux";
+import {
+  toggleGroupDatePickerVisible,
+  setGroupStartDate,
+  setGroupEndDate,
+  setGroupTitle,
+  setGroupDescription,
+  toggleCreateGroupVisible
+} from "../action";
+
+class CreateGroup extends React.Component {
+  state = {
+    currentlySetting: ""
+  };
+  _showDatePicker = target => {
+    this.setState({ currentlySetting: target });
+    this.props.toggleGroupDatePickerVisible();
+  };
+  _handleDatePicked = date => {
+    this.state.currentlySetting === "start"
+      ? this.props.setGroupStartDate(date)
+      : this.props.setGroupEndDate(date);
+    this.props.toggleGroupDatePickerVisible();
+  };
+  handleSave = () => {
+    const {
+      groupEndDate,
+      groupStartDate,
+      groupTitle,
+      groupDescription
+    } = this.props;
+    console.log(groupEndDate, groupStartDate, groupTitle, groupDescription);
+  };
+
+  render() {
+    const {
+      groupEndDate,
+      groupStartDate,
+      setGroupTitle,
+      setGroupDescription,
+      toggleCreateGroupVisible
+    } = this.props;
+    return (
+      <Card title="Create Trip">
+        <Text
+          onChangeText={text => setGroupTitle(text)}
+          style={styles.inputTitle}
+        >
+          {" "}
+          Title{" "}
+        </Text>
+        <TextInput
+          style={styles.title}
+          placeholder="Title"
+          onChangeText={text => setGroupTitle(text)}
+        />
+        <Text style={styles.inputTitle}> Description </Text>
+        <TextInput
+          style={styles.description}
+          placeholder="Description"
+          multiline={true}
+          onChangeText={text => setGroupDescription(text)}
+        />
+        <Text style={styles.inputTitle}> Date Range </Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Start Date"
+            buttonStyle={styles.button}
+            onPress={() => this._showDatePicker("start")}
+          />
+          <Button
+            title={
+              groupStartDate
+                ? moment(groupStartDate).format("MMMM DD, YYYY")
+                : ""
+            }
+            type="outline"
+            buttonStyle={styles.button}
+            disabled={true}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="End Date"
+            buttonStyle={styles.button}
+            onPress={() => this._showDatePicker("end")}
+          />
+          <Button
+            title={
+              groupEndDate ? moment(groupEndDate).format("MMMM DD, YYYY") : ""
+            }
+            type="outline"
+            buttonStyle={styles.button}
+            disabled={true}
+          />
+        </View>
+        <DateTimePicker
+          mode="date"
+          isVisible={this.props.groupDatePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={this.props.toggleGroupDatePickerVisible}
+        />
+        <View style={styles.buttonContainerBottom}>
+          <Button
+            buttonStyle={styles.buttonBottom}
+            title="Save"
+            onPress={this.handleSave}
+          />
+          <Button
+            buttonStyle={styles.buttonBottom}
+            title="Cancel"
+            onPress={toggleCreateGroupVisible}
+          />
+        </View>
+      </Card>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   button: {
@@ -30,9 +130,20 @@ const styles = StyleSheet.create({
     height: 40,
     marginTop: 20
   },
+  buttonBottom: {
+    width: 140,
+    height: 40,
+    marginTop: 20,
+    backgroundColor: "#008080"
+  },
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between"
+  },
+  buttonContainerBottom: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 50
   },
   description: {
     marginLeft: 10,
@@ -45,3 +156,43 @@ const styles = StyleSheet.create({
     marginLeft: 10
   }
 });
+
+const mapStateToProps = state => ({
+  groupDatePickerVisible: state.groupDatePickerVisible,
+  groupStartDate: state.groupStartDate,
+  groupEndDate: state.groupEndDate,
+  groupTitle: state.groupTitle,
+  groupDescription: state.groupDescription
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleGroupDatePickerVisible: () => {
+    const action = toggleGroupDatePickerVisible();
+    dispatch(action);
+  },
+  setGroupStartDate: date => {
+    const action = setGroupStartDate(date);
+    dispatch(action);
+  },
+  setGroupEndDate: date => {
+    const action = setGroupEndDate(date);
+    dispatch(action);
+  },
+  setGroupDescription: text => {
+    const action = setGroupDescription(text);
+    dispatch(action);
+  },
+  setGroupTitle: text => {
+    const action = setGroupTitle(text);
+    dispatch(action);
+  },
+  toggleCreateGroupVisible: () => {
+    const action = toggleCreateGroupVisible();
+    dispatch(action);
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateGroup);
